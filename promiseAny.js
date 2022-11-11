97ed7b3d7c69798ab0fd9848580c30cec22a6963
romka008 <romka008@yandex.ru>
2022-11-11 11:17:23 +0300

    add promiseAny

---
+++ promiseAny.js
@@ -0,0 +1,45 @@
+const p1 = Promise.resolve(3);
+const p2 = 1337;
+const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("foo");
+    }, 100);
+});
+
+Promise.any([p1, p2, p3])
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+
+promiseMyAny([p1, p2, p3])
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+
+function promiseMyAny(promises) {
+    return new Promise((res, rej) => {
+        let count = 0;
+        for (let i = 0; i < promises.length; i++) {
+            const el = promises[i];
+            Promise.resolve(el)
+                .then(data => {
+                    res(data);
+                })
+                .catch(err => {
+                    count++;
+                    if (count === promises.length) {
+                        rej("All promises were rejected");
+                    }
+                });
+        }
+    });
+}
+
+const p4 = Promise.reject(3);
+const p5 = Promise.reject(1337);
+const p6 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject("foo");
+    }, 100);
+});
+promiseMyAny([p4, p5, p6])
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
